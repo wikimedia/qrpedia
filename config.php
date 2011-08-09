ea5643eef2f2387740a4d0e4184c32c0b8b6ffd3
terence.eden@gmail.com <terence.eden@gmail.com@150620df-644e-ba33-b7d4-5fd2b1296e96>
2011-08-09 14:37:49 +0000

    Update the log writing
    git-svn-id: http://qrwp.googlecode.com/svn/trunk@14 150620df-644e-ba33-b7d4-5fd2b1296e96

--- config.php
+++ config.php
@@ -7,12 +7,15 @@
 
 	// Default Language
 	$default_language = "en";
-
-	// Write a log file entry for each visitor
-	$myFile = "log.txt";
-	$fh = fopen($myFile, 'a+');
-	// Tab separated. Date/Time	User Agent	IP Address	Language	Server requested	Page requested
-	$stringData = date("d/m/y H:i:s") . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\t" . $_SERVER["REMOTE_ADDR"] . "\t" . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "\t" . $_SERVER['SERVER_NAME']. "\t" .  $_GET['title'] . "\n";
-	fwrite($fh, $stringData);
-	fclose($fh);
+	
+	function writeLog($Redirected_URL)
+	{
+		// Write a log file entry for each visitor
+		$myFile = "log.txt";
+		$fh = fopen($myFile, 'a+');
+		// Tab separated. Date/Time	User Agent	IP Address	Language	Server requested	Page requested
+		$stringData = date("d/m/y H:i:s") . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\t" . $_SERVER["REMOTE_ADDR"] . "\t" . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "\t" . $_SERVER['SERVER_NAME']. "\t" .  $_GET['title'] . "\t" . $Redirected_URL . "\n";
+		fwrite($fh, $stringData);
+		fclose($fh);
+	}
 ?>
