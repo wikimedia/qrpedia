d73bcc430de7ece5ab26120b72daf4597b1c091d
terence.eden@gmail.com <terence.eden@gmail.com@150620df-644e-ba33-b7d4-5fd2b1296e96>
2011-08-09 14:50:25 +0000

    Added database logging
    git-svn-id: http://qrwp.googlecode.com/svn/trunk@15 150620df-644e-ba33-b7d4-5fd2b1296e96

--- config.php
+++ config.php
@@ -17,5 +17,26 @@
 		$stringData = date("d/m/y H:i:s") . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\t" . $_SERVER["REMOTE_ADDR"] . "\t" . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "\t" . $_SERVER['SERVER_NAME']. "\t" .  $_GET['title'] . "\t" . $Redirected_URL . "\n";
 		fwrite($fh, $stringData);
 		fclose($fh);
+		
+		//	Add logging to MySQL database
+		$mySQL_username="";
+		$mySQL_password="";
+		$mySQL_database="";
+
+		mysql_connect(localhost,$mySQL_username,$mySQL_password);
+		@mysql_select_db($mySQL_database) or die( "Unable to select database");
+
+		$query = "INSERT INTO `".$mySQL_database."`.`stats` (`Datetime`, `UA`, `IP`, `Languages`, `Domain`, `Path`, `Destination`) VALUES ("
+					. "'" . date("y-m-d H:i:s") 
+					. "', '" . $_SERVER['HTTP_USER_AGENT'] 
+					. "', '" . $_SERVER["REMOTE_ADDR"]
+					. "', '" . $_SERVER['HTTP_ACCEPT_LANGUAGE']
+					. "', '" . $_SERVER['SERVER_NAME']
+					. "', '" . $_GET['title']
+					. "', '" . $Redirected_URL
+					. "');";
+
+		$result = mysql_query($query);
+		
 	}
 ?>
