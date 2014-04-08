ace6fdfcfa1a77284218c8eada9e65775b115cbf
addshore <addshorewiki@gmail.com>
2014-04-08 21:51:41 +0100

    Format PHP code per MW conventions
    Change-Id: I17c5d39e9ac0915ab40e465ad86395b06140d97c

--- config.php
+++ config.php
@@ -8,38 +8,35 @@
 	// Default Language
 	$default_language = "en";
 	//	Add logging to MySQL database
-	$mySQL_username="";
-	$mySQL_password="";
-	$mySQL_database="";	
+	$mySQL_username = "";
+	$mySQL_password = "";
+	$mySQL_database = "";
 
-	function writeLog($Redirected_URL)
-	{
+	function writeLog( $Redirected_URL ) {
 		//	Add logging to MySQL database
-		$mySQL_username="";
-		$mySQL_password="";
-		$mySQL_database="";
+		$mySQL_username = "";
+		$mySQL_password = "";
+		$mySQL_database = "";
 
 		//	Connect to database
-		$mysqli = new mysqli('localhost', $mySQL_username, $mySQL_password, $mySQL_database);
-	
-		/* check connection */
-		if (!mysqli_connect_errno()) 
-		{
-			/* create a prepared statement */
-			if ($stmt = $mysqli->prepare(							 
-					"INSERT INTO `stats`	(`Datetime`,	`UA`,	`IP`,	`Languages`,	`Domain`,	`Path`,	`Destination`) 
-									VALUES 	(?,				?,		?,		?,				 	?,				?,			?)"
-												)
-				)
-			{
-				$stmt->bind_param('sssssss', $datetime, $ua, $ip, $languages, $domain, $path, $destination);
+		$mysqli = new mysqli( 'localhost', $mySQL_username, $mySQL_password, $mySQL_database );
 
-				$datetime = date("Y-m-d H:i:s");
+		/* check connection */
+		if ( !mysqli_connect_errno() ) {
+			/* create a prepared statement */
+			if ( $stmt = $mysqli->prepare(
+				"INSERT INTO `stats`(`Datetime`,`UA`,`IP`,`Languages`,`Domain`,`Path`,`Destination`)
+					VALUES (?,?,?,?,?,?,?)"
+			)
+			) {
+				$stmt->bind_param( 'sssssss', $datetime, $ua, $ip, $languages, $domain, $path, $destination );
+
+				$datetime = date( "Y-m-d H:i:s" );
 				$ua = $_SERVER['HTTP_USER_AGENT'];
 				$ip = $_SERVER["REMOTE_ADDR"];
 				$languages = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 				$domain = $_SERVER['SERVER_NAME'];
-				$path = stripslashes($_GET['title']);
+				$path = stripslashes( $_GET['title'] );
 				$destination = $Redirected_URL;
 
 				/* execute prepared statement */
@@ -50,11 +47,11 @@
 
 				// Write a log file entry for each visitor
 				$myFile = "log.txt";
-				$fh = fopen($myFile, 'a+');
-				// Tab separated. Date/Time	User Agent	IP Address	Language	Server requested	Page requested
-				$stringData = $datetime . "\t" . $ua . "\t" . $ip . "\t" . $languages . "\t" . $domain . "\t" .  $path . "\t" . $destination . "\n";
-				fwrite($fh, $stringData);
-				fclose($fh);
+				$fh = fopen( $myFile, 'a+' );
+				// Tab separated. Date/Time, User Agent, IP Address, Language, Server requested, Page requested
+				$stringData = $datetime . "\t" . $ua . "\t" . $ip . "\t" . $languages . "\t" . $domain . "\t" . $path . "\t" . $destination . "\n";
+				fwrite( $fh, $stringData );
+				fclose( $fh );
 			}
 		}
 	}
--- index.php
+++ index.php
@@ -1,35 +1,33 @@
 <?php
 	include "config.php";
-	
-	function getLanguageNameFromCode($code)
-	{
+
+	function getLanguageNameFromCode( $code ) {
 		global $mySQL_username;
 		global $mySQL_password;
 		global $mySQL_database;
-	
+
 		//	Connect to database
-		$mysqli = new mysqli('localhost', $mySQL_username, $mySQL_password, $mySQL_database);
+		$mysqli = new mysqli( 'localhost', $mySQL_username, $mySQL_password, $mySQL_database );
 
 		/* check connection */
-		if (!mysqli_connect_errno()) 
-		{
+		if ( !mysqli_connect_errno() ) {
 			/* change character set to utf8 */
-			$mysqli->set_charset("utf8"); 
-			
+			$mysqli->set_charset( "utf8" );
+
 			/* create a prepared statement */
-			if ($stmt = $mysqli->prepare("SELECT LanguageName
-															FROM lang
-															WHERE `Code`=?"
-												))
-			{
+			if ( $stmt = $mysqli->prepare( "SELECT LanguageName
+												FROM lang
+												WHERE `Code`=?"
+			)
+			) {
 				/* bind parameters for markers */
-				$stmt->bind_param("s", $code);
+				$stmt->bind_param( "s", $code );
 
 				/* execute query */
 				$stmt->execute();
 
 				/* bind result variables */
-				$stmt->bind_result($language_name);
+				$stmt->bind_result( $language_name );
 
 				/* fetch values */
 				$stmt->fetch();
@@ -38,74 +36,66 @@
 				$stmt->close();
 			}
 			return $language_name;
-		}
-		else	//	If the DB is not available, display the language code
+		} else //	If the DB is not available, display the language code
 		{
 			return $code;
 		}
-	}	
+	}
 
 	// An .htaccess file changes example.com/Foo to example.com/?title=foo
 	// Remove any escaped characters. Eg \'
-	$request = stripslashes($_GET['title']);
+	$request = stripslashes( $_GET['title'] );
 
 	// If a request has been sent - redirect the user	
-	if ($request != null)
-	{
+	if ( $request != null ) {
 		// Gets the phone user's primary language - based on the headers of the phone's browser
 		// Code modified from http://www.php.net/manual/en/reserved.variables.server.php#94237
 		// RFC 2616 compatible Accept Language Parser
 		// http://www.ietf.org/rfc/rfc2616.txt, 14.4 Accept-Language, Page 104
 		// Hypertext Transfer Protocol -- HTTP/1.1
 
-		foreach (explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']) as $lang) 
-		{
-			$pattern = 	'/^(?P<primarytag>[a-zA-Z]{2,8})'.
-							'(?:-(?P<subtag>[a-zA-Z]{2,8}))?(?:(?:;q=)'.
-							'(?P<quantifier>\d\.\d))?$/';
+		foreach ( explode( ',', $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) as $lang ) {
+			$pattern = '/^(?P<primarytag>[a-zA-Z]{2,8})' .
+				'(?:-(?P<subtag>[a-zA-Z]{2,8}))?(?:(?:;q=)' .
+				'(?P<quantifier>\d\.\d))?$/';
 
 			$splits = array();
-		
-			if (preg_match($pattern, $lang, $splits)) 
-			{
+
+			if ( preg_match( $pattern, $lang, $splits ) ) {
 				$phone_language = $splits[primarytag];
 				// Once the language has been found - no need to continue the loop.
 				break;
-			} 
+			}
 		}
 
 		// Get the language requested. For example fr.qrwp.org/foo assumes that /foo is French
 		$requested_server = $_SERVER['SERVER_NAME'];
 
-		if ($requested_server != $server_name) // If this has a subdomain
+		if ( $requested_server != $server_name ) // If this has a subdomain
 		{
-			$pieces = explode(".", $requested_server);
+			$pieces = explode( ".", $requested_server );
 			$requested_language = $pieces[0]; // Assume that only one sub domain has been chosen. "fr.en.de.qrwp.org" will return "fr"
-		}
-		else	//	If there is no sub domain, use the default language set in config.php
+		} else //	If there is no sub domain, use the default language set in config.php
 		{
 			$requested_language = $default_language;
 		}
 
 		// If the phone hasn't sent through a language header - set it to the requested language
-		if ($phone_language == null)
-		{
+		if ( $phone_language == null ) {
 			$phone_language = $requested_language;
 		}
 
 		//	The Norwegian language code is... complex!
 		//	See http://code.google.com/p/qrwp/issues/detail?id=4 for a discussion
-		if ($phone_language == "nb")
-		{
+		if ( $phone_language == "nb" ) {
 			$phone_language = "no";
 		}
 
 		//	If the phone's language is the same as the requested language (eg en-gb & en.qrwp) do the redirection without a call to Wikipedia 
-		if ($phone_language == $requested_language)
-		{
+		if ( $phone_language == $requested_language ) {
 			$mobile_url = "http://$requested_language.m.wikipedia.org/wiki/" . $request;
-			writeLog(urldecode($mobile_url));
-			header("Location: $mobile_url");
+			writeLog( urldecode( $mobile_url ) );
+			header( "Location: $mobile_url" );
 			exit;
 		}
 
@@ -125,46 +115,42 @@
 		$api_call = "http://$requested_language.wikipedia.org/w/api.php?action=query&prop=info|langlinks&lllimit=200&llurl&titles=$request&redirects=&format=json";
 
 		// Use CURL to retrieve the information
-		$curl_handle=curl_init();
-		curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
+		$curl_handle = curl_init();
+		curl_setopt( $curl_handle, CURLOPT_RETURNTRANSFER, 1 );
 		// Set a user agent with contact information so Wikipedia Admins can see who is using the service
-		curl_setopt($curl_handle, CURLOPT_USERAGENT, $user_agent);
-		curl_setopt($curl_handle,CURLOPT_URL,$api_call);
-		$response = curl_exec($curl_handle);
-		$response_info=curl_getinfo($curl_handle);
-		curl_close($curl_handle);
+		curl_setopt( $curl_handle, CURLOPT_USERAGENT, $user_agent );
+		curl_setopt( $curl_handle, CURLOPT_URL, $api_call );
+		$response = curl_exec( $curl_handle );
+		$response_info = curl_getinfo( $curl_handle );
+		curl_close( $curl_handle );
 
 		// Decode the JSON into an array
-		$results = json_decode($response,true);
+		$results = json_decode( $response, true );
 
 		// We need to find the ID of the page
 		$page_id_array = $results['query']['pages'];
-		$page_id = key($page_id_array);
+		$page_id = key( $page_id_array );
 
 		//If there is no $page_id it means a 404
-		if ($page_id != -1)
-		{
+		if ( $page_id != -1 ) {
 			// Find out how many links were returned
 			$links_array = $results['query']['pages'][$page_id]['langlinks'];
 
 			// Itterate through the array
-			for ($i = 0; $i <	count($links_array); $i++)
-			{
+			for ( $i = 0; $i < count( $links_array ); $i++ ) {
 				// Get the language
 				$article_language = $results['query']['pages'][$page_id]['langlinks'][$i]['lang'];
 
 				// If the language matches - perform the redirection
-								//	Catalan Fix
+				//	Catalan Fix
 				// Catalan isn't well supported on the phone.  Many phones don't have it as an option.
 				//	If the URL is ca.qrwp then we do the following
 				// 	If the phone is set to CA - send the article
 				//		Else, show the language select screen
 
-				if ($article_language == $phone_language) 
-				{
+				if ( $article_language == $phone_language ) {
 
-					if ($requested_language == "ca" && $phone_language == "es")
-					{
+					if ( $requested_language == "ca" && $phone_language == "es" ) {
 						//	Catalan Fix
 						break;
 					}
@@ -173,15 +159,14 @@
 					$article_url = $results['query']['pages'][$page_id]['langlinks'][$i]['url'];
 
 					// Quick and dirty search and replace to convert the URL into a mobile version
-					$mobile_url = str_replace('.wikipedia.org', '.m.wikipedia.org', $article_url);
-					$mobile_url = utf8_decode($mobile_url);
-					writeLog($mobile_url);
-					header("Location: $mobile_url");
+					$mobile_url = str_replace( '.wikipedia.org', '.m.wikipedia.org', $article_url );
+					$mobile_url = utf8_decode( $mobile_url );
+					writeLog( $mobile_url );
+					header( "Location: $mobile_url" );
 					exit;
 				}
 			}
-		}
-		else	//	404
+		} else //	404
 		{
 			//	Something has gone wrong
 			//	The article was not found in the requested Wikipedia.
@@ -192,46 +177,44 @@
 			//	This is a pretty rare edge case
 			// Write 404 into the log
 			//	Send them to their native Wikipedia so they can search for themselves
-			
+
 			//writeLog("404");
 			//header("Location: http://$phone_language.m.wikipedia.org/");
 			//exit;
 		}
 
-
 		//Minority Language / Missing Language
 
 		//	An html list of articles - for use if a translation can't be found
 		// The first in the list will be the article in the requested languge
-		$article_list .= "<li>" . getLanguageNameFromCode($requested_language) . " - <a href=\"http://$requested_language.m.wikipedia.org/wiki/$request\">$request</a></li>\n";
-		
+		$article_list .= "<li>" . getLanguageNameFromCode( $requested_language ) . " - <a href=\"http://$requested_language.m.wikipedia.org/wiki/$request\">$request</a></li>\n";
+
 		// Find out how many links were returned
 		$links_array = $results['query']['pages'][$page_id]['langlinks'];
 
 		// Itterate through the array
-		for ($i = 0; $i <	count($links_array); $i++)
-		{
+		for ( $i = 0; $i < count( $links_array ); $i++ ) {
 			// Get the language
 			$article_language = $results['query']['pages'][$page_id]['langlinks'][$i]['lang'];
 
 			// Get the Wikipedia URL for the language
 			$article_url = $results['query']['pages'][$page_id]['langlinks'][$i]['url'];
-		
+
 			// Get the title of the article in the foreign language
 			$article_title = $results['query']['pages'][$page_id]['langlinks'][$i]['*'];
 
 			// Quick and dirty search and replace to convert the URL into a mobile version
-			$mobile_url = str_replace('.wikipedia.org', '.m.wikipedia.org', $article_url);
-			
+			$mobile_url = str_replace( '.wikipedia.org', '.m.wikipedia.org', $article_url );
+
 			//	Get the Language name
-			$language_name = getLanguageNameFromCode($article_language);
+			$language_name = getLanguageNameFromCode( $article_language );
 
 			//	Add to the HTML list
 			$article_list .= "<li>$language_name - <a href=\"$mobile_url\">$article_title</a></li>\n";
 		}
 
 		//	Print a list of the available articles and let the user choose
-		header('Content-Type: text/html;charset=utf-8'); //	We're returning lots of unicode, let's make sure the browser knows that
+		header( 'Content-Type: text/html;charset=utf-8' ); //	We're returning lots of unicode, let's make sure the browser knows that
 
 		//	HTML5 FTW!
 		echo "<!DOCTYPE html>
@@ -248,33 +231,30 @@
 		//	If the URL is ca.qrwp then we do the following
 		//		Show the language select screen
 
-		if ($requested_language == "ca" && $phone_language != "ca")
-		{
+		if ( $requested_language == "ca" && $phone_language != "ca" ) {
 			echo "CA: En quin idioma vols llegir aquest article?<br />
 					ES: ¿En qué idioma quieres leer este artículo?<br />
 					EN: Which language would you like to read this article in?<br />";
-		}
-		else
-		{
-			echo	"Sorry, but Wikipedia does not have the page \"$request\" in your language ($phone_language).<br />\n";
-			echo	"Please try reading the <a href=\"http://translate.google.com/translate?tl=$phone_language&u=http%3A%2F%2F$requested_language.m.wikipedia.org%2Fwiki%2F$request\">Google auto-translated version</a>.
+		} else {
+			echo "Sorry, but Wikipedia does not have the page \"$request\" in your language ($phone_language).<br />\n";
+			echo "Please try reading the <a href=\"http://translate.google.com/translate?tl=$phone_language&u=http%3A%2F%2F$requested_language.m.wikipedia.org%2Fwiki%2F$request\">Google auto-translated version</a>.
 					<br />
 					<br />
 					Or read in one of the following languages:
 					<br />\n";
 		}
 
-		echo	"<ul>
+		echo "<ul>
 					$article_list
 				</ul>";
 		echo "</body>
 			</html>";
-			
-		writeLog("NA");
-			
+
+		writeLog( "NA" );
+
 		exit;
 	}
 
 	// No request was sent - send them to the main page
-	header("Location: http://qrpedia.org/");
+	header( "Location: http://qrpedia.org/" );
 	exit;
